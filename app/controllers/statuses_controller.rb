b572ca33326280a239eb18ff85a27f6568e476bf
Zlerp <zhpdeals@gmail.com>
2015-07-16 12:16:21 -0400

    pagination for statuses

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,6 +1,9 @@
 class StatusesController < ApplicationController
   def index
-    @statuses = Status.all     
+    @page = (params[:page] || 1).to_i  # Params is a hash of paramaters. parts of the URL.
+                                    # set the default page to page 1.
+    offset = (@page - 1) * 25    # This will skip 25 from this page.
+    @statuses = Status.order(created_at: :desc).limit(25).offset(offset).all     # This will limit the amount of statuses to 25 shown per page.
   end
 
   def show
